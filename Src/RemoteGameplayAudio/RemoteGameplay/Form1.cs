666f6c76e4e7fdeed7629b5a503bd94649ded2c1
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-05-14 08:38:50 +0200

    Fix sound

--- Src/RemoteGameplayAudio/RemoteGameplay/Form1.cs
+++ Src/RemoteGameplayAudio/RemoteGameplay/Form1.cs
@@ -94,6 +94,7 @@ namespace RemoteGameplay
             soundOut = new WasapiOut(wasapi, AudioClientShareMode.Shared, false, 2);
             src = new BufferedWaveProvider(WaveFormat.CreateCustomFormat(waveformat.Encoding, waveformat.SampleRate, waveformat.Channels, waveformat.AverageBytesPerSecond, waveformat.BlockAlign, waveformat.BitsPerSample));
             src.DiscardOnBufferOverflow = true;
+            src.BufferDuration = TimeSpan.FromMilliseconds(80);
             src.BufferLength = waveformat.AverageBytesPerSecond * 80 / 1000;
             soundOut.Init(src);
             soundOut.Play();
